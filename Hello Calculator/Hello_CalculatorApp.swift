1bd5067458acd5d6dc958eec0ce837756819c3b0
Micah779 <contactmaldrich@gmail.com>
2023-09-17 21:26:02 -0400

    Initial Commit

---
+++ Hello Calculator/Hello_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_CalculatorApp.swift
+//  Hello Calculator
+//
+//  Created by Micah Aldrich on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
